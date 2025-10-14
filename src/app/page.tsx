ef837e10626b8abca0872c4743de99e1b9d3b864
gt3oliveira <gt3oliveira@gmail.com>
2025-10-13 21:30:08 -0300

    feat: add first discount banner to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="p-5">
+      <Image
+        src="/banner-55-off.svg"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        // sizes="100vw"
+        alt="Banner 55% off"
+      />
     </div>
   );
 }
