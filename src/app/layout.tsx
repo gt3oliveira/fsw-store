49279b6330bd0eec76f2776e21fc7b2fedfb61a0
gt3oliveira <gt3oliveira@gmail.com>
2025-10-14 17:17:35 -0300

    feat: add footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { AuthProvider } from "@/providers/auth";
+import { Footer } from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <AuthProvider>
           <Header />
           {children}
+          <Footer />
         </AuthProvider>
       </body>
     </html>
---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-xs opacity-75">
+      &copy; {new Date().getFullYear()} - Copyright <strong>FSW Store</strong>
+    </div>
+  );
+};
