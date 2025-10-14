0535c99f695522ff199a15a10e63935fbc26bd37
gt3oliveira <gt3oliveira@gmail.com>
2025-10-14 16:44:13 -0300

    feat: add BannerFones and SectionList Mouses

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -21,6 +21,14 @@ export default async function Home() {
     },
   });
 
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
     <div className="flex flex-col space-y-6 p-5">
       <PromoBanner
@@ -39,12 +47,22 @@ export default async function Home() {
 
       <PromoBanner
         src="/banner-mouses.svg"
-        alt="até 55% de descontos em mouses"
+        alt="até 55% de descontos em Mouses"
       />
 
       <div className="-mx-5 space-y-2">
         <SectionTitle>teclados</SectionTitle>
         <ProductList products={keyboards} />
+      </div>
+
+      <PromoBanner
+        src="/banner-fones.svg"
+        alt="até 20% de descontos em Fones"
+      />
+
+      <div className="-mx-5 space-y-2">
+        <SectionTitle>mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   );
