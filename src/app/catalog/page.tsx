786676476561c4aabc4e61daa6c0a03bd2e395d2
gt3oliveira <gt3oliveira@gmail.com>
2025-10-14 18:44:28 -0300

    feat: add Catalog Page and Category-List-Items component

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,28 @@
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import { CategoryItem } from "./_components/category-item";
+
+export default async function CatalogPage() {
+  const categories = await prismaClient.category.findMany();
+
+  return (
+    <div className="flex flex-col space-y-8 p-5">
+      <Badge
+        variant={"outline"}
+        className="border-primary rounded-full border-2 px-3 py-[0.375rem] text-base uppercase"
+      >
+        <p>
+          <ShapesIcon />
+        </p>
+        Catálogo
+      </Badge>
+
+      <div className="grid w-full grid-cols-2 gap-6">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </div>
+  );
+}
