a8b965ba15a991b50ff510ed51c15ae8a0181ba0
gt3oliveira <gt3oliveira@gmail.com>
2025-10-15 14:21:45 -0300

    feat: garantir que o usuário seja redirecionado para a rota do link ao clicar na categoria

--- src/app/catalog/_components/category-item.tsx
+++ src/app/catalog/_components/category-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -7,20 +8,22 @@ interface CategoryItemProps {
 
 export const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="flex flex-col">
-      <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-gradient-to-tr from-[#5033c3] to-[#5033c3]/20">
-        <Image
-          src={category.imageUrl}
-          alt={category.name}
-          width={0}
-          height={0}
-          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
-          sizes="100vw"
-        />
+    <Link href={`/category/${category.slug}`}>
+      <div className="flex flex-col">
+        <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-gradient-to-tr from-[#5033c3] to-[#5033c3]/20">
+          <Image
+            src={category.imageUrl}
+            alt={category.name}
+            width={0}
+            height={0}
+            className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+            sizes="100vw"
+          />
+        </div>
+        <div className="bg-accent rounded-br-lg rounded-bl-lg py-3 text-center">
+          <p className="text-sm font-semibold">{category.name}</p>
+        </div>
       </div>
-      <div className="bg-accent rounded-br-lg rounded-bl-lg py-3 text-center">
-        <p className="text-sm font-semibold">{category.name}</p>
-      </div>
-    </div>
+    </Link>
   );
 };
