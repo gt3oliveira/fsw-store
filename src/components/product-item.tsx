0773020eb00475817e18adda9e9b21d0e2d9bd8f
gt3oliveira <gt3oliveira@gmail.com>
2025-10-15 15:01:58 -0300

    feat: garantir que usuário seja direcionado para ProductDetailPage

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Badge } from "./ui/badge";
 import { ArrowDownIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
@@ -11,45 +12,49 @@ interface ProductItemProps {
 }
 export const ProductItem = ({ product, className }: ProductItemProps) => {
   return (
-    <div className="mx-2 flex flex-col">
-      <div
-        className={cn(
-          "bg-accent relative flex size-44 items-center justify-center rounded-lg",
-          className,
-        )}
-      >
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute top-3 left-3 rounded-full">
-            <ArrowDownIcon />
-            {product.discountPercentage}%
-          </Badge>
-        )}
-        <Image
-          src={product.imageUrls[0]}
-          alt={product.name}
-          width={0}
-          height={0}
-          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
-          sizes="100vw"
-        />
-      </div>
-      <div>
-        <p className="mt-2 line-clamp-1 text-sm">{product.name}</p>
-      </div>
-      <div className="flex items-center gap-2">
-        {product.discountPercentage > 0 ? (
-          <>
+    <Link href={`/product/${product.slug}`}>
+      <div className="mx-2 flex flex-col">
+        <div
+          className={cn(
+            "bg-accent relative flex size-44 items-center justify-center rounded-lg",
+            className,
+          )}
+        >
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute top-3 left-3 rounded-full">
+              <ArrowDownIcon />
+              {product.discountPercentage}%
+            </Badge>
+          )}
+          <Image
+            src={product.imageUrls[0]}
+            alt={product.name}
+            width={0}
+            height={0}
+            className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+            sizes="100vw"
+          />
+        </div>
+        <div>
+          <p className="mt-2 line-clamp-1 text-sm">{product.name}</p>
+        </div>
+        <div className="flex items-center gap-2">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="font-semibold">
+                {formatCurrency(product.totalPrice)}
+              </p>
+              <p className="text-[11px] line-through opacity-75">
+                {formatCurrency(Number(product.basePrice))}
+              </p>
+            </>
+          ) : (
             <p className="font-semibold">
               {formatCurrency(product.totalPrice)}
             </p>
-            <p className="text-[11px] line-through opacity-75">
-              {formatCurrency(Number(product.basePrice))}
-            </p>
-          </>
-        ) : (
-          <p className="font-semibold">{formatCurrency(product.totalPrice)}</p>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
