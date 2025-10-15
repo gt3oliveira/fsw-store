0f611257d90b8c0c20e3de993fce9869d42ca623
gt3oliveira <gt3oliveira@gmail.com>
2025-10-15 14:23:28 -0300

    feat: add Category Page Slug, constants types and CategoryItems

--- src/app/(home)/_components/category-item.tsx
+++ src/app/(home)/_components/category-item.tsx
@@ -1,35 +1,22 @@
 import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
-import {
-  HeadphonesIcon,
-  KeyboardIcon,
-  MonitorIcon,
-  MouseIcon,
-  SpeakerIcon,
-  SquareIcon,
-} from "lucide-react";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
 }
 
 export const CategoryItem = ({ category }: CategoryItemProps) => {
-  const categoryIcon = {
-    keyboards: <KeyboardIcon size={16} />,
-    monitors: <MonitorIcon size={16} />,
-    headphones: <HeadphonesIcon size={16} />,
-    mousepads: <SquareIcon size={16} />,
-    speakers: <SpeakerIcon size={16} />,
-    mouses: <MouseIcon size={16} />,
-  };
-
   return (
-    <Badge
-      variant={"outline"}
-      className="flex w-full items-center justify-center rounded-lg py-3"
-    >
-      <p>{categoryIcon[category.slug as keyof typeof categoryIcon]}</p>
-      <span className="text-xs font-semibold">{category.name}</span>
-    </Badge>
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant={"outline"}
+        className="flex w-full items-center justify-center rounded-lg py-3"
+      >
+        <p>{CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}</p>
+        <span className="text-xs font-semibold">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,46 @@
+import { ProductItem } from "@/components/product-item";
+import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/category-icon";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { prismaClient } from "@/lib/prisma";
+
+export default async function CategoryProducts({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const category = await prismaClient.category.findFirst({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      products: true,
+    },
+  });
+
+  if (!category) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col space-y-8 p-5">
+      <Badge
+        variant={"outline"}
+        className="border-primary rounded-full border-2 px-3 py-[0.375rem] text-base uppercase"
+      >
+        <p>{CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}</p>
+        {category.name}
+      </Badge>
+
+      <div className="grid w-full grid-cols-2 gap-x-1 gap-y-6">
+        {category.products.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={computeProductTotalPrice(product)}
+            className="h-[176px] w-full"
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
