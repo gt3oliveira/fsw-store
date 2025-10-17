2dd39e10f2e46e25bc079aefea9c3826cb4d542a
gt3oliveira <gt3oliveira@gmail.com>
2025-10-16 21:29:43 -0300

    feat: add sheet-menu-item _ link or onClick

---
+++ src/components/sheet-menu-item.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { SheetClose } from "./ui/sheet";
+
+interface SheetMenuItemProps {
+  children: React.ReactNode;
+  href?: string;
+  onclick?: () => void;
+}
+
+export const SheetMenuItem = ({ children, onclick }: SheetMenuItemProps) => {
+  return (
+    <SheetClose asChild>
+      <Button
+        onClick={onclick}
+        variant={"outline"}
+        className="w-full justify-start"
+      >
+        {children}
+      </Button>
+    </SheetClose>
+  );
+};
+
+export const SheetMenuItemLink = ({ children, href }: SheetMenuItemProps) => {
+  return (
+    <SheetClose asChild>
+      <Link href={href as string}>
+        <Button variant={"outline"} className="w-full justify-start">
+          {children}
+        </Button>
+      </Link>
+    </SheetClose>
+  );
+};
