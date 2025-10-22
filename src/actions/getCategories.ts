35c2dc95eddb48e65e0fa3127538ef994f122175
gt3oliveira <gt3oliveira@gmail.com>
2025-10-22 16:33:00 -0300

    feat: garantir a inclusão da id dos produtos

--- src/actions/getCategories.ts
+++ src/actions/getCategories.ts
@@ -2,5 +2,13 @@ import { prismaClient } from "@/lib/prisma";
 import { Category } from "@prisma/client";
 
 export const getCategories = async (): Promise<Category[]> => {
-  return await prismaClient.category.findMany();
+  return await prismaClient.category.findMany({
+    include: {
+      products: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
 };
