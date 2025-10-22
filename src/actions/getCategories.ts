dfe15839e3d49ba39737023fc49b41905a7718ec
gt3oliveira <gt3oliveira@gmail.com>
2025-10-22 17:26:00 -0300

    chore: get-categories find many

--- src/actions/getCategories.ts
+++ src/actions/getCategories.ts
@@ -2,13 +2,5 @@ import { prismaClient } from "@/lib/prisma";
 import { Category } from "@prisma/client";
 
 export const getCategories = async (): Promise<Category[]> => {
-  return await prismaClient.category.findMany({
-    include: {
-      products: {
-        select: {
-          id: true,
-        },
-      },
-    },
-  });
+  return await prismaClient.category.findMany();
 };
