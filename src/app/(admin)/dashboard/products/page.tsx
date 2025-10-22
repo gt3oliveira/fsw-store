08f1422934caf1c294b62d8d4ba034d42ec5880e
gt3oliveira <gt3oliveira@gmail.com>
2025-10-21 22:21:11 -0300

    feat: add botão de adicionar produtos e a quantidade de produtos existentes

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,12 +1,34 @@
 import { getProducts } from "@/actions/getProducts";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { PackageIcon, PlusIcon } from "lucide-react";
 
 export default async function ProductsPage() {
   const products = await getProducts();
   return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id}>{product.name}</div>
-      ))}
+    <div className="flex w-full flex-col space-y-10 p-10">
+      <Badge variant={"heading"}>
+        <p>
+          <PackageIcon size={20} />
+        </p>
+        Produtos
+      </Badge>
+
+      <div className="flex items-center justify-between">
+        <span className="text-lg font-semibold">
+          Total de produtos: {products.length}
+        </span>
+        <Button>
+          <PlusIcon />
+          Adicionar Produto
+        </Button>
+      </div>
+
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>{product.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
